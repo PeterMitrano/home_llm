25ab3864df100495e904210ed3e1333377a59ca0
peter <peter@T15>
2024-07-14 19:42:13 -0400

    initial prototyping. the model doesn't use the lifx API correctly, and the lifx API itself seems possibly a bit unreliable?

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ That LLM will start a new chat every time, and receive the API docs for that dev
 That complicates allowing the system to learn though... if we want to teach the device-LLM, we need to change its prompt instead of relying on ICL.
 So that means the user or maybe the main LLM will need to be able to adjust the prompt used by the device LLM.
 Anyway, the main LLM will have a basic system prompt to start each new interaction.
+
+Since the coding LLM will initially make mistakes on unfamiliar devices, we need a way to correct it.
+I think we can consider two types of errors, one which is code that fails to run, and one that runs but does the wrong thing.
+The user will need to help correct when it does the wrong thing, but code failures we can probably resolve autonomously.
+To resolve cod errors, we could try to iteratively feed in the error the code produces, and get a new response.
+Once it succeeds, we can treat this as a new training example and run fine-tuning.
+GPT4All doesn't include finetuning as far as I can tell, so I'll have to figure out some way to do that later.
--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import lifxlan
-
-from gpt4all import GPT4All
-
 SEPERATOR = "\n---\n"
 
 FIXED_PROMPT = """
@@ -16,22 +12,22 @@ Use the return value of the code to come up with the language response which ans
 
 CODE_FIXED_PROMPT = """
 You are a home assistant device.
-You will be asked questions, and you should respond only python code, with all import statements needed.
+You will be asked questions, and you should respond with only python code, with all import statements needed.
+Your respond will be directly exec'd.
+If it throws an error you will receive the error message and fix it. In this case, again only respond with code.
 Do not include any comments in the code or your response.
 The return value of the code should be a dictionary.
 """
 
-
 API_DOCS = """
-API Documentation:
-
+lifxlan API Documentation:
+lan = LifxLAN() # example
 # color is a list of HSBK values: [hue (0-65535), saturation (0-65535), brightness (0-65535), Kelvin (2500-9000)]
-# duration is the transition time in milliseconds
+# times are in milliseconds
 # name is the string label for the light, such as "Right Lamp"
 # names is a list of name strings, such as ["Left Lamp", "Right Lamp"]
 # group is a string label for a group, such as "Living Room"
 # location is the string label for a location, such as "My Home"
-
 get_lights()
 get_color_lights()
 get_multizone_lights()
@@ -46,11 +42,9 @@ get_power_all_lights()
 get_color_all_lights()
 Device API
 In keeping with the LIFX protocol, all lights are devices, and so support the following methods:
-
 # label is a string, 32 char max
 # power can be "on"/"off", True/False, 0/1, or 0/65535
 # arguments in [square brackets] are optional
-
 set_label(label)
 set_power(power)
 get_mac_addr()
@@ -90,76 +84,75 @@ supports_color()
 supports_temperature()
 supports_multizone()
 Light API
-You can get Light objects automatically though LAN-based discovery (takes a few seconds), or by creating Light objects using a known MAC address and IP address:
-
-lights = lan.get_lights()                              # Option 1: Discovery
-light = Light("12:34:56:78:9a:bc", "192.168.1.42")     # Option 2: Direct
+lights = lan.get_lights()                              # Discovery
 The Light API provides everything in the Device API, as well as:
-
-# duration is the transition time in milliseconds
 # is_transient is 1/0. If 1, return to the original color after the specified number of cycles. If 0, set light to specified color
-# period is the length of one cycle in milliseconds
-
 set_power(power, [duration])
 set_color(color, [duration])
 get_power()
 get_color()
-The Light API also provides macros for basic colors, like RED, BLUE, GREEN, etc. Setting colors is as easy as mybulb.set_color(BLUE). See light.py for complete list of color macros.
-
-Finally, you can set parts of the color individually using the following four methods. However, the bulbs must receive all four values in each SetColor message. That means that using one of the following methods is always slower than using set_color(color) above because it will have to call get_color() first to get the other three values.
-
-set_hue(hue, [duration])
-set_brightness(brightness, [duration])
-set_saturation(saturation, [duration])
-set_colortemp(kelvin, [duration])
-
-Group API
-A Group is a collection of devices. Under the covers, a Group is just a list of device objects (like Devices, Lights, MultiZoneLights) and a set of functions that send multi-threaded commands to the applicable devices in the group. The multi-threading allows changes to be made more or less simultaneously. At the very least, it is certainly faster than if you looped through each individual light one at a time. You can get a Group by group, location, or device names via the LifxLAN API. However, you can also instantiate a Group with any arbitrary list of device objects. Here are some ways to create groups:
-
-# The following methods use discovery
-lan = LifxLAN()
-g = lan.get_devices_by_name(["Left Lamp", "Right Lamp"])
-g = lan.get_devices_by_group("Living Room")
-g = lan.get_devices_by_location("My Home")
-
-# This method is fastest
-right = Light("12:34:56:78:9a:bc", "192.168.0.2")
-left = Light("cb:a9:87:65:43:21", "192.168.0.3")
-g = Group([right, left])
-Almost all of the Group API methods are commands. Commands will only be sent to the devices in the group that support that capability. If you want to get state information from the devices, you will need to access the list of devices and call their get methods directly.
-
-add_device(device_object)
-remove_device(device_object)
-remove_device_by_name(device_name)
-get_device_list()
-set_power(power, [duration])
-set_color(color, [duration])
+The Light API also provides macros for basic colors, like RED, BLUE, GREEN, etc.
+You can set parts of the color individually using the following four methods.
+However, the bulbs must receive all four values in each SetColor message.
+That means that using one of the following methods is always slower than using set_color(color) above.
 set_hue(hue, [duration])
 set_brightness(brightness, [duration])
 set_saturation(saturation, [duration])
 set_colortemp(kelvin, [duration])
 """
 
+import lifxlan
+from lifxlan import LifxLAN, WorkflowException
+from gpt4all import GPT4All
+from lifxlan import Light, Device, Group, GetGroup
+
+
+def lifx_str(device):
+    indent = ""
+    s = ""
+    try:
+        device.refresh()
+        s += device.device_characteristics_str(indent)
+        s += indent + device.device_product_str(indent)
+    except WorkflowException:
+        pass
+    return s
+
+
 def main():
     code_model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf")  # TODO: is there a coding specific model?
     chat_model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf")
 
-    code_prompt = SEPERATOR.join([API_DOCS, CODE_FIXED_PROMPT])
+    # Get the current names, groups, locations,
+    lifx = lifxlan.LifxLAN(num_lights=3)
+    devices = lifx.get_devices()
+    device_descriptions = "\n".join([lifx_str(d) for d in devices])
 
-    # # Test the repeatability
-    # TEST_PROMPT = "Select 10 random numbers, with replacement, in the range 1-100."
-    # for i in range(10):
-    #     print(f"{i}:")
-    #     with model.chat_session():
-    #         print(model.generate(TEST_PROMPT))
+    code_system_prompt = SEPERATOR.join([API_DOCS,
+                                         f'Current devices:\n{device_descriptions}',
+                                         CODE_FIXED_PROMPT])
 
-    # Test whether a new "with model.chat_session" correctly "remembers" or "forgets"
-    # with model.chat_session():
-    #     model.generate("remember the number 4. When I ask you for my number, tell me it is 4.")
-    #     print(model.generate("Tell me my number."))
-    #
-    # with model.chat_session():
-    #     print(model.generate("Tell me my number."))
+    with code_model.chat_session(system_prompt=code_system_prompt):
+        code_prompt = "Turn on all the lights."
+        for _ in range(3):
+            try:
+                print("\nCODE PROMPT:")
+                print(code_prompt)
+                code_response = code_model.generate(code_prompt)
+                print("\nCODE RESPONSE:")
+                print(code_response)
+                exec(code_response)
+            except Exception as e:
+                code_prompt = f"Your code produced this error, please try again. You may choose to produce code that will help debug as well. \n{str(e)}"
+
+    # Once we've successfully executed the code, fine-tune the model so we don't make the same mistake again
+    lan = lifxlan.LifxLAN()
+    lights = lan.get_lights()
+    lan.get_devices_by_group("")
+    for light in lights:
+        if "Bedroom" in str(light):
+            device = Device(str(light))
+            device.set_power(True)
 
 
 if __name__ == '__main__':
